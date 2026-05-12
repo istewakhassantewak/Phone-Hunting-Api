8b0a7102506a029b0ef4c534fb5a6ce9855e13ce
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-05-13 05:14:08 +0600

    show details

--- js/phone.js
+++ js/phone.js
@@ -26,7 +26,6 @@ const displayPhones = (phones, isShowAll) => {
 
     phones.forEach(phone => {
         // create a div
-        console.log(phone)
         const phoneCard = document.createElement('div')
         phoneCard.classList = `card bg-gray-100  p-4 shadow-xl`;
         phoneCard.innerHTML = `<figure>
@@ -37,8 +36,8 @@ const displayPhones = (phones, isShowAll) => {
                         <h2 class="card-title">${phone.phone_name}</h2>
                         <p>A card component has a figure, a body part, and inside body there are title and actions parts
                         </p>
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-primary">Buy Now</button>
+                        <div class="card-actions justify-center">
+                            <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
                         </div>
                     </div>`
         phoneContainer.append(phoneCard)
@@ -46,6 +45,47 @@ const displayPhones = (phones, isShowAll) => {
     // hide loading spinner
     toggleLoadingSpinner(false);
 }
+
+
+// 
+const handleShowDetail = async (id) => {
+    //    load single phone data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data
+    showPhoneDetails(phone)
+}
+const showPhoneDetails = (phone) => {
+    console.log(phone)
+
+
+    const showDetailContainer = document.getElementById('show-detail-container');
+
+    showDetailContainer.innerHTML = `<div class="bg-gray-100 flex items-center justify-center py-5">
+    <img src="${phone.image}"  alt="phone">
+</div>
+                <h3 class="text-3xl font-bold">${phone?.name ?? 'not provided'}</h3>
+                <p class="font-medium text-lg text-gray-600"><span class="font-bold text-xl text-black">Storage:</span>
+                    ${phone?.mainFeatures?.storage ?? 'not provided'}</p>
+                <p class="font-medium text-lg text-gray-600"><span class="font-bold text-xl text-black">Display Size:</span>
+                    ${phone?.mainFeatures?.displaySize ?? 'not provided'}</p>
+                <p class="font-medium text-lg text-gray-600"><span class="font-bold text-xl text-black">Chipset:</span>
+                    ${phone?.mainFeatures?.chipSet ?? 'not provided'}</p>
+                <p class="font-medium text-lg text-gray-600"><span class="font-bold text-xl text-black">Memory:</span>
+                    ${phone?.mainFeatures?.memory ?? 'not provided'}</p>
+                <p class="font-medium text-lg text-gray-600"><span class="font-bold text-xl text-black">Slug:</span> ${phone?.slug ?? 'not provided'}
+                </p>
+                <p class="font-medium text-lg text-gray-600"><span class="font-bold text-xl text-black">Release Date:
+                    </span>${phone?.releaseDate ?? 'not provided'}</p>
+                <p class="font-medium text-lg text-gray-600"><span class="font-bold text-xl text-black">Brand:</span> ${phone?.brand ?? 'not provided'}
+                </p>
+                <p class="font-medium text-lg text-gray-600"><span class="font-bold text-xl text-black">GPS:</span> ${phone?.others?.GPS ?? 'not provided'}
+                </p> `
+
+
+    show_details_modal.showModal();
+}
+
 // handle search button
 const handleSearch = (isShowAll) => {
     toggleLoadingSpinner(true);
@@ -68,4 +108,4 @@ const handleShowAll = () => {
     handleSearch(true);
 }
 
-// loadPhone();
+loadPhone(13);
